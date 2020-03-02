330478c64e72bf77e4f6e49dc18a37c64ec0781e
syaronyan <navybeeshrimp@gmail.com>
2020-03-02 19:29:53 +0900

    #2レビュー修正

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -2,34 +2,17 @@
 @section('content')
 
     <div class="login">
-        <!-- <h2 class="active"> sign in </h2>
+        <form method='post' action=''>
+            <span>ログイン</span>
+            <input type="email" class="text" name="email" placeholder="email">
+            <input type="password" class="text" name="password" placeholder="password">
+            <input type="checkbox" id="checkbox-1-1" class="custom-checkbox">
+            <label for="checkbox-1-1">Keep me Signed in</label>
+            <button class="signin">
+            Sign In
+            </button>
+            <a href="#">Forgot Password?</a>
+        </form>
+    </div>
 
-        <h2 class="nonactive"> sign up </h2> -->
-    <form method='post' action=''>
-
-        <span>ログイン</span>
-        <input type="email" class="text" name="email" placeholder="email">
-
-    <br>
-    
-    <br>
-
-    <!-- <span>password</span> -->
-        <input type="password" class="text" name="password" placeholder="password">
-    <br>
-
-        <input type="checkbox" id="checkbox-1-1" class="custom-checkbox">
-        <label for="checkbox-1-1">Keep me Signed in</label>
-    
-        <button class="signin">
-        Sign In
-        </button>
-
-
-    <hr>
-
-    <a href="#">Forgot Password?</a>
-  </form>
-
-</div>
 @endsection
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,34 +1,18 @@
 @extends('layout.header')
 @section('content')
-<div class="login">
-        <!-- <h2 class="active"> sign in </h2>
-
-        <h2 class="nonactive"> sign up </h2> -->
-    <form method='post' action=''>
-
-        <span>登録</span>
-        <input type="email" class="text" name="email" placeholder="email">
-
-    <br>
-    
-    <br>
-
-    <!-- <span>password</span> -->
-        <input type="password" class="text" name="password" placeholder="password">
-    <br>
-
-        <input type="checkbox" id="checkbox-1-1" class="custom-checkbox">
-        <label for="checkbox-1-1">Keep me Signed in</label>
-    
-        <button class="signin">
-        Sign Up
-        </button>
-
-
-    <hr>
-
-    <a href="#">Forgot Password?</a>
-  </form>
-
-</div>
+    <div class="login">
+        <form method='post' action='' id='signup'>
+            <span>登録</span>
+            <input type="text" class="text" name="name" placeholder="name">
+            <input type="email" class="text" name="email" placeholder="email">
+            <input type="password" class="text" name="password" placeholder="password">
+            <input type="password" class="text" name="password" placeholder="password">
+            <input type="checkbox" id="checkbox-1-1" class="custom-checkbox">
+            <label for="checkbox-1-1">Keep me Signed in</label>
+            <button class="signin">
+                Sign In
+            </button>
+            <a href="#">Forgot Password?</a>
+        </form>
+    </div>
 @endsection
