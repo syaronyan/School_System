724a64818e59193aefa66d7f3786c847793f00fc
汐崎大樹 <shalo@shaloMBP.local>
2020-06-28 15:12:08 +0900

    ログイン時のバグを修正

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -55,7 +55,6 @@ class StudentsController extends Controller
 
     public function signin (Request $request) 
     {
-        
             $inputs = $request->all();
             //rules
             $rules = [
@@ -92,57 +91,59 @@ class StudentsController extends Controller
     public function ses_put (Request $request, $id) {
         $request->session()->put('user', $id);
 
-        $user = $request->session()->get('user');
-        if (!empty($user)){
-            $group_ids = [];
-            $group_names = [];
-            $group_imgs = [];
-            $parcents = [];
-            $progress_tasks_edit = [];
-            $student_id = $request->student_id;
+        // $user = $request->session()->get('user');
+        // if (!empty($user)){
+        //     $group_ids = [];
+        //     $group_names = [];
+        //     $group_imgs = [];
+        //     $parcents = [];
+        //     $progress_tasks_edit = [];
+        //     $student_id = $request->student_id;
 
-            //サブクエリ
-            $sub = Progress::select('tasks_id', 'check_flag')
-            ->where('student_id', '=', ':student_id')
-            ->where('check_flag', '=', ':check_flag')
-            ->toSql();
+        //     //サブクエリ
+        //     $sub = Progress::select('tasks_id', 'check_flag')
+        //     ->where('student_id', '=', ':student_id')
+        //     ->where('check_flag', '=', ':check_flag')
+        //     ->toSql();
 
-            //進捗管理のデータ取得
-            $progress_tasks = Tasks::select(DB::raw('group_id, group.img_link as group_img, group.name as group_name, count(*) as tasks_count, count(progress.check_flag) as progress_count'))
-            ->join('group', 'group.id', '=', 'tasks.group_id')
-            ->leftJoin(DB::raw('('.$sub.') AS progress'), 'progress.tasks_id', '=', 'tasks.id')
-            ->groupBy('tasks.group_id')
-            ->setBindings([':student_id'=>$user, ':check_flag'=>'1'])
-            ->get();
+        //     //進捗管理のデータ取得
+        //     $progress_tasks = Tasks::select(DB::raw('group_id, group.img_link as group_img, group.name as group_name, count(*) as tasks_count, count(progress.check_flag) as progress_count'))
+        //     ->join('group', 'group.id', '=', 'tasks.group_id')
+        //     ->leftJoin(DB::raw('('.$sub.') AS progress'), 'progress.tasks_id', '=', 'tasks.id')
+        //     ->groupBy('tasks.group_id')
+        //     ->setBindings([':student_id'=>$user, ':check_flag'=>'1'])
+        //     ->get();
             
-            //上記データを編集
-            foreach ($progress_tasks as $progress_task){
-                array_push($group_ids, $progress_task['group_id']);
-                array_push($group_names, $progress_task['group_name']);
-                array_push($group_imgs, $progress_task['group_img']);
-                $parsent = round(($progress_task['progress_count']/$progress_task['tasks_count'])*100);
-                array_push($parcents, $parsent);
+        //     //上記データを編集
+        //     foreach ($progress_tasks as $progress_task){
+        //         array_push($group_ids, $progress_task['group_id']);
+        //         array_push($group_names, $progress_task['group_name']);
+        //         array_push($group_imgs, $progress_task['group_img']);
+        //         $parsent = round(($progress_task['progress_count']/$progress_task['tasks_count'])*100);
+        //         array_push($parcents, $parsent);
 
-                $progress_tasks_edit['group_ids'] = $group_ids;
-                $progress_tasks_edit['group_names'] = $group_names;
-                $progress_tasks_edit['group_imgs'] = $group_imgs;
-                $progress_tasks_edit['parcents'] = $parcents;
-            }
+        //         $progress_tasks_edit['group_ids'] = $group_ids;
+        //         $progress_tasks_edit['group_names'] = $group_names;
+        //         $progress_tasks_edit['group_imgs'] = $group_imgs;
+        //         $progress_tasks_edit['parcents'] = $parcents;
+        //     }
 
-            //生徒の情報を取得
-            $student = Students::select('id', 'name', 'email')
-            ->where('id', '=', $user)
-            ->first();
+        //     //生徒の情報を取得
+        //     $student = Students::select('id', 'name', 'email', 'created_at')
+        //     ->where('id', '=', $user)
+        //     ->first();
 
             // var_dump($progress_tasks_edit);
             // exit;
 
             //進捗管理と生徒の情報を持ってmypageに遷移
-            return view('mypage', compact('progress_tasks_edit', 'student'));
+            // return view('mypage', compact('progress_tasks_edit', 'student'));
         // return view('mypage');
-    }   else{
-            return $this->index();
-    }
+        header( "Location: https://learning.techis.jp/mypage" ) ;
+        exit;
+    // }   else{
+    //         return $this->index();
+    // }
     }
 
     public function change_password (Request $request) {
@@ -193,6 +194,7 @@ class StudentsController extends Controller
     public function ses_del (Request $request) {
         // $request->session()->forget('user');
         $request->session()->flush();
-        return $this->index();
+        header( "Location: https://learning.techis.jp" ) ;
+        exit ;
     }
 }
